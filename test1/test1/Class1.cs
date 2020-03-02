09e75307cf23876118339227144d9867abdc90c8
arfaghif <arfasen17@gmail.com>
2020-03-02 20:44:33 +0700

    init

---
+++ test1/test1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace test1
+{
+	public class Class1
+	{
+		private int age;
+		public Class1()
+		{
+			age = 0;
+			Console.WriteLine("Hello NAufal!");
+		}
+	}
+}
---
+++ test1/test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test1
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Class1 C = new Class1();
+		}
+	}
+}
