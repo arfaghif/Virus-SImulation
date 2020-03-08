5d9e85f41bd8f02c2fae2dbdd4e6cf50dbfb1b70
muchammadibnu <47872314+muchammadibnu@users.noreply.github.com>
2020-03-09 05:36:22 +0700

    Update PairOfCity.cs

--- versiCMD/versiCMD/PairOfCity.cs
+++ versiCMD/versiCMD/PairOfCity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace versiCMD
 {
@@ -7,6 +7,19 @@ namespace versiCMD
 		private readonly City cityA;
 		private readonly City cityB;
 		private readonly int TrAB;
+		//private bool cek;
+		/*public bool Cek
+		 *{
+		 *	get
+		 *	{
+		 *		return cek;
+		 *	}
+		 *	set
+		 *	{
+		 *		cek=value;
+		 *	}
+		 *}
+		 */
 		public City CityA
 		{
 			get
@@ -19,6 +32,7 @@ namespace versiCMD
 			cityA = a;
 			cityB = b;
 			TrAB = tr;
+			//cek=false;
 		}
 		public double hitungSAB(int hari_awal, int hari_akhir)
 		{
@@ -29,7 +43,15 @@ namespace versiCMD
 		{
 			if (hitungSAB(hari_awal,hari_akhir) > 1)
 			{
-				return (int)Math.Floor((Math.Log((cityA.Populasi * TrAB - 1) / (cityA.Populasi - 1), Constant.euler) * -1 / Constant.gamma) + hari_awal) + 1;
+				double a= ((Math.Log((cityA.Populasi * TrAB - 1) / (cityA.Populasi - 1), Constant.euler) * -1 / Constant.gamma) + hari_awal) + 1;
+				int i;
+				for(i=1; i<=Math.Floor(a) + 1 ; i++){
+					if (i>a){
+						break;
+					}
+				}
+				//cityB.hariTerinfeksi=i+cityA.hariTerinfeksi;
+				return i;
 			}
 			else
 			{
