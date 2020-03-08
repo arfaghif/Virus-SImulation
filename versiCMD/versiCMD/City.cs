8bba038c6b223be6c65ee4565e97fe6cb70718bf
arfaghif <arfasen17@gmail.com>
2020-03-08 16:01:57 +0700

    city.cs

--- versiCMD/versiCMD/City.cs
+++ versiCMD/versiCMD/City.cs
@@ -4,8 +4,72 @@ namespace versiCMD
 {
 	public class City
 	{
-		public City()
+		private readonly string nama;
+		private readonly int populasi;
+		private string status;
+		public int Status
+        {
+			get
+			{
+				switch (status)
+				{
+					case "Belum diidentifikasi":
+						return 0;
+						break;
+					case "Sedang diidentifikasi":
+						return 1;
+						break;
+					case "Tidak Terinfeksi":
+						return 2;
+						break;
+					default:
+						return 3;
+						break;
+				}
+			}
+            
+			set
+            {
+					if (value >= 0 && value <= 3)
+					{
+						switch (value)
+						{
+							case 0:
+								status = "Belum diidentifikasi";
+								break;
+							case 1:
+								status = "Sedang diidentifikasi";
+								break;
+							case 2:
+								status = "Tidak Terinfeksi";
+								break;
+							case 3:
+								status = "Terinfeksi";
+								break;
+						}
+					}
+            }
+        }
+
+		public City(string nm, int pop)
 		{
+			nama = nm;
+			populasi = pop;
+			status = "Belum diidentifikasi";
 		}
+		public City(string nm, int pop, int stat) 
+		{
+			nama = nm;
+			populasi = pop;
+			Status = stat;
+		}
+		public void printCity()
+        {
+			Console.WriteLine(nama);
+			Console.WriteLine(populasi);
+			Console.WriteLine(status);
+        }
+		
+		
 	}
 }
--- versiCMD/versiCMD/Program.cs
+++ versiCMD/versiCMD/Program.cs
@@ -7,6 +7,11 @@ namespace versiCMD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            City c = new City("Bandung", 2000);
+            City c2 = new City("Jakarta", 4000, 3);
+            c.printCity();
+            c2.printCity();
+            Console.WriteLine(c.Name);
         }
     }
 }
